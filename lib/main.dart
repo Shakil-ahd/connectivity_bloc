cc7332dba3bbaa3214d8ddf3e342b7d77998d1c6
Shakil Ahmed <shakilahmed.of@gmail.com>
2025-10-05 11:50:50 +0600

    connectivity check

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:connectivity_bloc/homescreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Connnectivity',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
